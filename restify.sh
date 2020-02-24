da0e0b0b508a043d754907b070d7542d55affdde
joan <joan.herisson@univ-evry.fr>
2020-02-24 21:22:28 +0100

    .env location

--- restify.sh
+++ restify.sh
@@ -18,7 +18,7 @@ TOOL_TAG=master
 MODE=redis
 IMAGE_TAG=redis
 COMMAND=\"python3 ../tofill/test/RestQuery.py -file_1 input/<filename> -param_1 <param_1>\"
-TOOL="$tool_name > .env
+TOOL="$tool_name > tofill/.env
 
 
 echo "# $tool_name-rest
