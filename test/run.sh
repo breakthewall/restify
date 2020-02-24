339aeb610a335499292c1ef6d8abd83a4dcb3ac0
joan <joan.herisson@univ-evry.fr>
2020-02-24 22:46:39 +0100

    path

--- test/run.sh
+++ test/run.sh
@@ -7,6 +7,8 @@ mode=$1
 
 sleep 5
 
+source ../.env
+
 pip install --upgrade pip
 pip install requests
-python3 tool_RestQuery.py -file_1 <filename> -param_1 <param_1> -server_url http://${TOOL_REST}_${mode}:8888/REST
+python3 ../${TOOL}-rest/test/RestQuery.py -file_1 <filename> -param_1 <param_1> -server_url http://${TOOL_REST}_${mode}:8888/REST
