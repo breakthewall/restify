c83fca68ad58d5eede8153506beacc77393f1e5f
joan <joan.herisson@univ-evry.fr>
2020-02-24 22:18:43 +0100

    url parse

--- restify.sh
+++ restify.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-
-# Clone REST part
-git clone https://github.com/brsynth/rest.git
-
-# Clone tool part
 tool_url=$1
 basename=$(basename $tool_url)
 tool_name=${basename%.*}
+
+# Clone REST part
+git clone https://github.com/brsynth/rest.git
+# Clone tool part
 git clone $tool_url
+# Clone tool REST part
+tool_rest_url=`echo $tool_url | sed 's|\(.*\)/.*|\1|'`
+git clone $tool_rest_url"/"$tool_name"-rest.git"
 
 # Rename the current folder
 mv "$PWD" "${PWD%/*}/${tool_name}-rest"
